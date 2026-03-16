56019bde17ab126153be5b04d398238f5790ef9f
Sapinmoney <sapinsreekuttan57@gmail.com>
2026-03-16 08:57:28 +0000

    Initial commit: Azure VNET module and environments

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "1daa6582-ee59-493a-b337-66da04511d5c"
+}
+
